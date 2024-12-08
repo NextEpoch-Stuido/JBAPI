2a5cc794b70a9cd9fde2ab6b792bbc5dae71af01
卡尔·弗雷利特 <2033200993@qq.com>
2024-12-08 19:51:54 +0800

    Add files via upload

---
+++ 宇宙无敌超级神威霸王龙SL的实用API/Game/Baozi.cs
@@ -0,0 +1,14 @@
+﻿namespace JBAPI.Baozi
+{
+    public class baozi
+    {
+        /// <summary>
+        /// 包子是扫比
+        /// </summary>
+        /// <returns></returns>
+        public static string Whatshappen()
+        {
+            return "包子是扫比";
+        }
+    }
+}
