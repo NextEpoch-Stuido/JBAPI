c6e98ecbdb865544f0d185b8c5b73334cd30ed2a
卡尔·弗雷利特 <2033200993@qq.com>
2025-03-16 16:05:18 +0800

    这才是真正的JBAPI

---
+++ JBAPI/API/Features/Path.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace JBAPI.API.Features
+{
+    public static class Paths
+    {
+        static Paths() => Reload();
+
+        /// <summary>
+        /// 游戏文件依赖
+        /// </summary>
+        public static string ManagedAssemblies { get; } = Path.Combine(Path.Combine(Environment.CurrentDirectory, "SiteWinter_Data"), "Managed");
+
+        /// <summary>
+        /// 插件文件夹
+        /// </summary>
+        public static string PluginFile { get; } = Directory.GetParent(Path.Combine(Environment.CurrentDirectory, "SiteWinter_Data")).FullName;
+
+        /// <summary>
+        /// 配置文件
+        /// </summary>
+        public static string? Config = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SiteWinter", "PluginConfig");
+
+        /// <summary>
+        /// 插件
+        /// </summary>
+        public static string? Plugins { get; set; }
+
+        /// <summary>
+        /// 依赖文件夹
+        /// </summary>
+        public static string? Dependencies { get; set; }
+
+        /// <summary>
+        /// 加载插件与依赖文件夹
+        /// </summary>
+        public static void Reload()
+        {
+            Plugins = Path.Combine(PluginFile, "Plugins");
+            Dependencies = Path.Combine(Plugins, "dependencies");
+            Config = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SiteWinter", "PluginConfig");
+        }
+    }
+}
---
+++ JBAPI/API/Features/Player.cs
@@ -0,0 +1,11 @@
+﻿using GameSystem.Player;
+using JBAPI.API.Config;
+using JBAPI.API.Enums;
+
+namespace JBAPI.API.Features
+{
+    public static class Player
+    {
+        // 开始征途吧
+    }
+}
